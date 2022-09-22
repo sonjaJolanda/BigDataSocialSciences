74a4972718d2ef092c27e882e9b43a3ce4f74615
Sonja Klein <so161kle@htwg-konstanz.de>
2022-09-22 08:24:17 +0200

    week 4 done

---
+++ week4/week4_own.py
@@ -0,0 +1,15 @@
+[x for x in range(1, 10)]
+
+[x for x in range(1, 10) if x % 2 == 0]
+
+
+my_list = [1, 2, 3]
+
+x = []
+for a in my_list:
+    x.append(a * 2)
+
+x = [a * 2 for a in my_list] # this is the same thing as the one above
+
+
+
