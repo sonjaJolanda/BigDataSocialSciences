edf2b08c31d32de6ea4bf12987a1db9be5d2f74d
Sonja Klein <so161kle@htwg-konstanz.de>
2022-10-14 05:32:43 +0200

    updated week2

--- week2/week2_own.py
+++ week2/week2_own.py
@@ -1,13 +1,7 @@
-import pandas as pd
-quotes_df = pd.DataFrame(data = quotes, columns=["quotes"])
-print(quotes_df)
-#----------------------------------------------
 import requests
-page = requests.get("http://quotes.toscrape.com/")
-print(page.content)
-#----------------------------------------------
 from bs4 import BeautifulSoup as bs
-soup = bs(page.content, 'html.parser')
+page = requests.get("http://quotes.toscrape.com/") # Sends a GET request. Returns:response-object
+soup = bs(page.content, 'html.parser') # constructor, needs itself and the markup-parser ToDo Remember this
 print(soup)
 soup.head
 soup.footer
@@ -17,21 +11,15 @@ print(soup.prettify())
 # soup.find_all('span', class='text') !!!!!!!!!
 print(soup.find('span', class_='text'))  #(element name, attributes) -> i think
 print(soup.find_all('span', class_='text'))
-print(soup.find_all('span', itemprop='text'))
-#----------------------------------------------
 txt = soup.find_all('span', itemprop='text')
-print(txt)
-#----------------------------------------------
-for i in txt:
-    print(i.get_text())
 #----------------------------------------------
 quotes = []
-for i in txt:
+for i in txt: # there is no normal for loop in python
     quotes.append((i.get_text()))
 #----------------------------------------------
 import pandas as pd
-quotes_df = pd.DataFrame(data=quotes, columns=["quotes"])
-quotes_df.to_excel('../week2/quotes_excel_sheet_week2.xlsx')
+quotes_df = pd.DataFrame(data=quotes, columns=["quotes"]) # create a DF
+quotes_df.to_excel('../week2/quotes_excel_sheet_week2.xlsx') # convert into excel file
 #----------------------------------------------
 authors_txt = soup.find_all('small', class_='author')
 authors = []
@@ -40,17 +28,14 @@ for i in authors_txt:
 authors_df = pd.DataFrame(data=authors, columns=["authors"])
 authors_df.to_excel('../week2/authors_excel_sheet_week2.xlsx')
 #----------------------------------------------
-quotes_and_authors_df = pd.concat([quotes_df, authors_df], axis=1)
-quotes_and_authors_df
+quotes_and_authors_df = pd.concat([quotes_df, authors_df], axis=1) # concat two DFs
 quotes_and_authors_df.to_excel('../week2/quotes_and_authors_excel_sheet_week2.xlsx')
 #----------------------------------------------
-quotes_and_authors_dic = dict(zip(authors, quotes))
-print(quotes_and_authors_dic)
+quotes_and_authors_dic = dict(zip(authors, quotes)) # make a dictionary out of two DFs
 df_dict = pd.DataFrame(quotes_and_authors_dic, index=[0])
 df_dict.to_excel('../week2/q_a_dict_excel_sheet_week2.xlsx')
-
-from xlwt import Workbook
-wb = Workbook()
+#----------------------------------------------
+from xlwt import Workbook as wb
 sheet1 = wb.add_sheet('Sheet from the dictionary')
 
 counter = 1;
@@ -58,5 +43,4 @@ for key, value in quotes_and_authors_dic.items():
     sheet1.write(counter, 0, key)
     sheet1.write(counter, 1, value)
     counter += 1
-
 wb.save('xlwt q_a_dict_excel_sheet_week2.xlsx')
