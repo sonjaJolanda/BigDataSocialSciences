0a251d357f94b6a7c4d2006ad4af95405a3c83b2
Sonja Klein <so161kle@htwg-konstanz.de>
2022-10-14 05:43:46 +0200

    updated week2

--- week2/week2_own.py
+++ week2/week2_own.py
@@ -1,25 +1,31 @@
 import requests
-from bs4 import BeautifulSoup as bs
 page = requests.get("http://quotes.toscrape.com/") # Sends a GET request. Returns:response-object
-soup = bs(page.content, 'html.parser') # constructor, needs itself and the markup-parser ToDo Remember this
-print(soup)
-soup.head
-soup.footer
-soup.body
-#--
-print(soup.prettify())
-# soup.find_all('span', class='text') !!!!!!!!!
-print(soup.find('span', class_='text'))  #(element name, attributes) -> i think
-print(soup.find_all('span', class_='text'))
+# page # <Repoinse [200]>
+# print(page.content) # the whole html
+#-------------------------------------------------------
+from bs4 import BeautifulSoup as bs # import BeautifulSoup library
+soup = bs(page.content, 'html.parser')
+# -> get parsed version of our data using HTML parser
+# -> constructor, needs itself and the markup-parser ToDo Remember this
+
+print(soup) # compared to the non parsed version this now ToDo find differences
+# soup.head # navigate through the tree. (or soup.footer or soup.body)
+# print(soup.prettify()) # compared to the non prettified version this now ToDo find differences
+#-------------------------------------------------------
+# soup.find_all('span', class='text') # this throws an error! -> use underscore
+print(soup.find('span', class_='text'))  # (element name, attributes) -> i think
+# print(soup.find_all('span', class_='text'))  # (element name, attributes) -> i think
+# print(soup.find_all('span', itemprop='text')) # same thing
 txt = soup.find_all('span', itemprop='text')
-#----------------------------------------------
+#-------------------------------------------------------
 quotes = []
 for i in txt: # there is no normal for loop in python
     quotes.append((i.get_text()))
-#----------------------------------------------
-import pandas as pd
-quotes_df = pd.DataFrame(data=quotes, columns=["quotes"]) # create a DF
-quotes_df.to_excel('../week2/quotes_excel_sheet_week2.xlsx') # convert into excel file
+#-------------------------------------------------------
+import pandas as pd # use pandas library to make our variable into a dataframe
+quotes_df = pd.DataFrame(data=quotes, columns=["quotes"])
+quotes_df.to_excel('../week2/quotes_excel_sheet_week2.xlsx') # export df to excel
+
 #----------------------------------------------
 authors_txt = soup.find_all('small', class_='author')
 authors = []
@@ -28,14 +34,16 @@ for i in authors_txt:
 authors_df = pd.DataFrame(data=authors, columns=["authors"])
 authors_df.to_excel('../week2/authors_excel_sheet_week2.xlsx')
 #----------------------------------------------
-quotes_and_authors_df = pd.concat([quotes_df, authors_df], axis=1) # concat two DFs
+quotes_and_authors_df = pd.concat([quotes_df, authors_df], axis=1) # axis=1 -> horizontally
 quotes_and_authors_df.to_excel('../week2/quotes_and_authors_excel_sheet_week2.xlsx')
 #----------------------------------------------
-quotes_and_authors_dic = dict(zip(authors, quotes)) # make a dictionary out of two DFs
+# Another way is to use dictionaries:
+quotes_and_authors_dic = dict(zip(authors, quotes))
 df_dict = pd.DataFrame(quotes_and_authors_dic, index=[0])
 df_dict.to_excel('../week2/q_a_dict_excel_sheet_week2.xlsx')
-#----------------------------------------------
-from xlwt import Workbook as wb
+from xlwt import Workbook
+
+wb = Workbook()
 sheet1 = wb.add_sheet('Sheet from the dictionary')
 
 counter = 1;
@@ -43,4 +51,5 @@ for key, value in quotes_and_authors_dic.items():
     sheet1.write(counter, 0, key)
     sheet1.write(counter, 1, value)
     counter += 1
+
 wb.save('xlwt q_a_dict_excel_sheet_week2.xlsx')
