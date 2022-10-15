6fa1506d9d24fce88e18e897d65d83311b2b9916
Sonja Klein <so161kle@htwg-konstanz.de>
2022-10-15 10:58:13 +0200

    updated week7

--- week7/week7_own.py
+++ week7/week7_own.py
@@ -1,6 +1,3 @@
-names = ['Heidi', 'John', 'Jenny']
-print("-and-".join(names)) # Heidi-and-John-and-Jenny
-# --------------------------------------------------------------
 name = 'Heidi'
 name.isalpha() # False if there were 0-9
 age = '22'
@@ -13,7 +10,9 @@ m = lambda x: x+10
 print(m(3)) # 13
 # ToDo the apply function + the lambda function (apply is part of pandas because its a DF function
 # --------------------------------------------------------------
-
+names = ['Heidi', 'John', 'Jenny']
+print("-and-".join(names)) # Heidi-and-John-and-Jenny
+# --------------------------------------------------------------
 
 
 
