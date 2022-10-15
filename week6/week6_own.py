ea27ca7c63cbee7cf9ec95238b847d4f3a6b3fc8
Sonja Klein <so161kle@htwg-konstanz.de>
2022-10-15 09:20:47 +0200

    updated week6

--- week6/week6_own.py
+++ week6/week6_own.py
@@ -1,3 +1,16 @@
+names = ['Heidi', 'John', 'Jenny']
+print("-and-".join(names)) # Heidi-and-John-and-Jenny
+# --------------------------------------------------------------
+name = 'Heidi'
+name.isalpha() # False if there were 0-9
+age = '22'
+age.isdigit() # False if there were a-z
+nameAndAge = 'Heidi22'
+nameAndAge.isalnum() # can be both
+# ----- lambda -------------------------------------------------
+m = lambda x: x+10
+print(m(3)) # 13
+# --------------------------------------------------------------
 import nltk
 import pandas as pd
 sentences = """
