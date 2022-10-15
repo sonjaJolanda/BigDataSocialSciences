4dd1fe554fbc3aed7a21561daeb7578a90112a48
Sonja Klein <so161kle@htwg-konstanz.de>
2022-10-15 10:11:37 +0200

    added week7

--- week7/week7_own.py
+++ week7/week7_own.py
@@ -7,9 +7,16 @@ age = '22'
 age.isdigit() # False if there were a-z
 nameAndAge = 'Heidi22'
 nameAndAge.isalnum() # can be both
+# ToDo everything above is not part of the midterm, thats why I haven't included it in the summary
 # ----- lambda -------------------------------------------------
 m = lambda x: x+10
 print(m(3)) # 13
+# ToDo the apply function + the lambda function (apply is part of pandas because its a DF function
 # --------------------------------------------------------------
+
+
+
+
+
 
 
